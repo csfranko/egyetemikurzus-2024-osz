9024c22ba995c6ad9c16ead396c243311747d681
Franko Csaba Andras <csabafranko2001@gmail.com>
2024-11-18 17:27:55 +0100

    Projekt setup, kezdetleges struktúra kialakítása

---
+++ J2WL0G/StudentRegistrySystem/Model/SubjectGrades.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRegistrySystem.Model
+{
+    public class SubjectGrades
+    {
+        public string SubjectGrade { get; set; }
+        public List<int> Grades { get; set; }
+
+        public SubjectGrades() { }
+    }
+}
---
+++ J2WL0G/StudentRegistrySystem/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            //todo
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+       // Console.WriteLine("Hello, World!");
+    }
+}
